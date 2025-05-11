9f8bd5b7dec87fb6235f4879c0fc374155815850
emagood <52244807+emagood@users.noreply.github.com>
2025-05-11 17:22:57 -0300

    Update plugin.gd

--- addons/mesh_merger/plugin.gd
+++ addons/mesh_merger/plugin.gd
@@ -1,4 +1,4 @@
 @tool
 extends EditorPlugin
-
+#https://github.com/tomankirilov/MeshMerger/tree/Godot-4.0
 #This plugin is just a simple class. This script only exists so it can be stored as a plug-in and not take space.
